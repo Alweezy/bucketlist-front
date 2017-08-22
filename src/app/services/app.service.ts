53304a270d2d6aac410d9d638b142abd2bbb7f26
Alweezy <alvinmutisya@gmail.com>
2017-08-22 20:30:40 +0300

    [Feature]Implement bucketlist and item services

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -94,3 +94,138 @@ export class AuthenticateService {
       });
   }
 }
+
+@Injectable()
+export class BuckeListService {
+
+  url = 'https://buck3tlist.herokuapp.com/api/v1/bucketlists/'
+
+  constructor(private http: Http) {}
+
+  // create a bucket list
+  createBucketList(name: string) {
+    const token = localStorage.getItem('current_user');
+    const headers = new Headers({ 'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token});
+    const options = new RequestOptions({ headers: headers });
+    return this.http.post(this.url, JSON.stringify({'name': name}), options)
+      .map(response => response.json())
+      .catch(errors => {
+        return Observable.throw(errors.json())
+      });
+  }
+
+  // get all bucket lists
+  getAllBucketLists(page, limit, search) {
+    limit = limit || 10;
+    page = page || 1;
+    const token = localStorage.getItem('current_user');
+    const headers = new Headers({ 'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token});
+    const options = new RequestOptions({ headers: headers });
+    return this.http.get(
+      this.url + '?page=' + page + '&limit=' + limit + '&q=' + search, options)
+      .map(response => response.json())
+      .catch(errors => {
+        return Observable.throw(errors.json())
+      });
+  }
+
+  // get bucket lists with names containing the value of 'q'
+  searchBucketLits(search, limit) {
+    if (/^[0-9]+$/.test(search)) {
+      search = JSON.stringify(search);
+    }
+    limit = limit || 10;
+    const token = localStorage.getItem('current_user');
+    const headers = new Headers({ 'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token});
+    const options = new RequestOptions({ headers: headers });
+    return this.http.get(this.url + '?q=' + search + '&limit=' + limit, options)
+      .map(response => response.json())
+      .catch(errors => {
+        return Observable.throw(errors.json())
+      });
+  }
+
+  // update bucket list
+  updateBucketLists(bucketListId: number, name: string) {
+    const token = localStorage.getItem('current_user');
+    const headers = new Headers({ 'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token});
+    const options = new RequestOptions({ headers: headers });
+    return this.http.put(this.url + bucketListId, JSON.stringify({'name': name}), options)
+      .map(response => response.json())
+      .catch(errors => {
+        return Observable.throw(errors.json())
+      });
+  }
+
+  // update bucket list
+  deleteBucketLists(bucketListId: number) {
+    const token = localStorage.getItem('current_user');
+    const headers = new Headers({ 'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token});
+    const options = new RequestOptions({ headers: headers });
+    return this.http.delete(this.url + bucketListId, options)
+      .map(response => response.json())
+      .catch(errors => {
+        return Observable.throw(errors.json())
+      });
+  }
+
+}
+
+@Injectable()
+export class BucketListItemService {
+
+  url = 'https://buck3tlist.herokuapp.com/api/v1/bucketlists/'
+
+  constructor(private http: Http) {}
+
+  // create bucket list item
+  createItem(bucketListId: number, name: string) {
+    const token = localStorage.getItem('current_user');
+    const headers = new Headers({ 'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token});
+    const options = new RequestOptions({ headers: headers });
+    return this.http.post(this.url + bucketListId + '/items/', JSON.stringify({'name': name}), options)
+      .map(response => response.json())
+      .catch(errors => {
+        return Observable.throw(errors.json())
+      });
+  }
+
+  // create bucket list item
+  updateItem(bucketListId: number, itemId: number, editedField: string, editedValue: any) {
+    const token = localStorage.getItem('current_user');
+    let data = {}
+    if (editedField === 'name') {
+      data = {'name': editedValue }
+    }else{
+      data = {'name': '', 'done': editedValue}
+    }
+    const headers = new Headers({ 'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token});
+    const options = new RequestOptions({ headers: headers });
+    return this.http.put(this.url + bucketListId + '/items/' + itemId,
+      JSON.stringify(data), options)
+      .map(response => response.json())
+      .catch(errors => {
+        return Observable.throw(errors.json())
+      });
+  }
+
+  // delete bucket list item
+  deleteItem(bucketListId: number, itemId: number) {
+    const token = localStorage.getItem('current_user');
+    const headers = new Headers({ 'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token});
+    const options = new RequestOptions({ headers: headers });
+    return this.http.delete(this.url + bucketListId + '/items/' + itemId, options)
+      .map(response => response.json())
+      .catch(errors => {
+        return Observable.throw(errors.json())
+      });
+  }
+}
